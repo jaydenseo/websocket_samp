c38ba7351e9d97ab75474c59d4f423de74f9e2cd
jaydenseo <jayden.seo@osckorea.com>
2022-11-03 14:44:52 +0900

    웹소켓통신 handshakeInteceptor 구현 (클라이언트 ip 정보 처리)

--- src/main/java/com/example/ccfe2/config/WebSocketConfig.java
+++ src/main/java/com/example/ccfe2/config/WebSocketConfig.java
@@ -3,7 +3,13 @@ package com.example.ccfe2.config;
 import java.util.Map;
 import java.util.Objects;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.config.ChannelRegistration;
@@ -13,9 +19,12 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -34,14 +43,36 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
         // 웹소켓 연결을 하는 경우, endpoint가 되는 url (ws://localhost:9091/websocket)
-        // stomp 5버전 이상부터는 
         registry.addEndpoint("/websocket")
-                .setAllowedOriginPatterns("*");
+                .setAllowedOriginPatterns("*")
+                .setHandshakeHandler(null)
+                .addInterceptors(handshakeInterceptor());
 
         // SockJs로 웹소켓 연결을 하는 경우, endpoint가 되는 url (http://localhost:9091/websocket)
         registry.addEndpoint("/websocket")
                 .setAllowedOriginPatterns("*")
-                .withSockJS();            
+                // .addInterceptors(handshakeInterceptor())
+                .withSockJS()
+                .setInterceptors(handshakeInterceptor()); // SockJS의 경우, .addInterceptors 대신 .setInterceptors를 사용할 수도 있다.  
+    }
+
+    @Bean
+    public HandshakeInterceptor handshakeInterceptor() {
+        return new HandshakeInterceptor() {
+            @Override
+            public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+                if (request instanceof ServletServerHttpRequest) {
+                    ServletServerHttpRequest servletServerRequest = (ServletServerHttpRequest) request;
+                    HttpServletRequest servletRequest = servletServerRequest.getServletRequest();
+
+                    attributes.put("remoteAddr", servletRequest.getRemoteAddr());
+                }
+                return true;
+            }
+            @Override
+            public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+            }
+        };
     }
 
     @Override
@@ -51,46 +82,45 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
             public Message<?> preSend(Message<?> message, MessageChannel channel) {
                 StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
                 String sessionId = accessor.getSessionId();
+                String remoteAddr = Objects.toString(accessor.getSessionAttributes().get("remoteAddr"), "");
 
                 if(StompCommand.CONNECT.equals(accessor.getCommand())){
                     // StompHeaders headers = StompHeaders.readOnlyStompHeaders(accessor.toNativeHeaderMap());
                     String userId = Objects.toString(accessor.getFirstNativeHeader("userId"), "");
                     String hostIp = Objects.toString(accessor.getFirstNativeHeader("hostIp"), "");
-                    String connectTime = Objects.toString(accessor.getFirstNativeHeader("connectTime"), "");
-                    String requestUrl = Objects.toString(accessor.getFirstNativeHeader("requestUrl"), "");
                     
                     Map<String, Object> map = accessor.getSessionAttributes();
                     map.put("userId", userId);
                     map.put("hostIp", hostIp);
-                    map.put("connectTime", connectTime);
-                    map.put("requestUrl", requestUrl);
                     accessor.setSessionAttributes(map);
 
-                    log.info("[ClientInboundChannel] [CONNECT] [{} | {} | {}]" + " >>> " + accessor.getSessionAttributes().toString(), sessionId, userId, hostIp);
+                    log.info("[ClientInboundChannel] [CONNECT] [{} | {} | {} | {}]" + " >>> " + accessor.getSessionAttributes().toString(), sessionId, userId, hostIp, remoteAddr);
                 }
                 else if(StompCommand.SUBSCRIBE.equals(accessor.getCommand())){
 
                     String userId = accessor.getSessionAttributes().get("userId").toString();
                     String hostIp = accessor.getSessionAttributes().get("hostIp").toString();
 
-                    log.info("[ClientInboundChannel] [SUBSCRIBE] [{} | {} | {}]" + " >>> " + accessor.getDestination(), sessionId, userId, hostIp);
+                    log.info("[ClientInboundChannel] [SUBSCRIBE] [{} | {} | {} | {}]" + " >>> " + accessor.getDestination(), sessionId, userId, hostIp, remoteAddr);
                 }
                 else if(StompCommand.SEND.equals(accessor.getCommand())){
 
                     String userId = accessor.getSessionAttributes().get("userId").toString();
                     String hostIp = accessor.getSessionAttributes().get("hostIp").toString();
                     String messagePayload = new String((byte[])message.getPayload());
-                    log.info("[ClientInboundChannel] [SEND] [{} | {} | {}]" + " >>> " + messagePayload, sessionId, userId, hostIp);
+                    log.info("[ClientInboundChannel] [SEND] [{} | {} | {} | {}]" + " >>> " + messagePayload, sessionId, userId, hostIp, remoteAddr);
                 }
                 else if(StompCommand.DISCONNECT.equals(accessor.getCommand())){
 
                     String userId = accessor.getSessionAttributes().get("userId").toString();
                     String hostIp = accessor.getSessionAttributes().get("hostIp").toString();
                     String messagePayload = new String((byte[])message.getPayload());
-                    log.info("[ClientInboundChannel] [DISCONNECT] [{} | {} | {}]" + " >>> " + accessor.getSessionAttributes().toString(), sessionId, userId, hostIp);
+                    log.info("[ClientInboundChannel] [DISCONNECT] [{} | {} | {} | {}]" + " >>> " + accessor.getSessionAttributes().toString(), sessionId, userId, hostIp, remoteAddr);
                 }
                 return message;                
             }
         });
     }
+
+
 }
